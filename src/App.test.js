04b449831f037c4fe27bef6d028df86a1dbfb61c
josepdominguez <jdomin123@gmail.com>
2022-04-09 13:51:14 +0200

    Renaming some components pt3

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Avengers Movies section', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Avengers Movies/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Movie Sources section', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Movie Sources/i);
   expect(linkElement).toBeInTheDocument();
 });
