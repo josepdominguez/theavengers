d5d07f86fabaa3ed0c0280c17c32a59b5d191e0c
josepdominguez <jdomin123@gmail.com>
2022-04-09 13:14:32 +0200

    Deploy my App

---
+++ precache-manifest.de67eda572cb03276b775103e2bd6813.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0236391deb83fa23b00fc4a1d92b1e42",
+    "url": "/theavengers/index.html"
+  },
+  {
+    "revision": "eaf5eb0e6f20f04e7723",
+    "url": "/theavengers/static/css/2.709e72d0.chunk.css"
+  },
+  {
+    "revision": "cf6d3d4fc2805538ddb4",
+    "url": "/theavengers/static/css/main.65a00d12.chunk.css"
+  },
+  {
+    "revision": "eaf5eb0e6f20f04e7723",
+    "url": "/theavengers/static/js/2.f52d8963.chunk.js"
+  },
+  {
+    "revision": "15b07506049fb98da101702aa72f9177",
+    "url": "/theavengers/static/js/2.f52d8963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6d3d4fc2805538ddb4",
+    "url": "/theavengers/static/js/main.a568e3d1.chunk.js"
+  },
+  {
+    "revision": "1ff7d915e1dca38e8e9d",
+    "url": "/theavengers/static/js/runtime-main.3894b43b.js"
+  },
+  {
+    "revision": "837710f5f22579e7abbf31acaeb188ee",
+    "url": "/theavengers/static/media/video.837710f5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ed1c718385e08ea252d553a1a726431.js"
+  "/theavengers/precache-manifest.de67eda572cb03276b775103e2bd6813.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/theavengers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
